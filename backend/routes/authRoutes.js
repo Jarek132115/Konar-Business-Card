8a100edb7566145005e181ea07e7388fb3ec5719
Parth <darjiparth13@gmail.com>
2025-05-23 08:51:14 +0530

    Working on users Reviews section

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,41 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors')
-const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController')
+const cors = require('cors');
+const {
+  test,
+  registerUser,
+  loginUser,
+  getProfile,
+  logoutUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController');
 
-const userController = require('../controllers/userProfileController');
+const serviceController = require('../controllers/serviceController');
+const workController = require('../controllers/workController');
+const reviewController = require('../controllers/reviewController');
 
-//middleware
+
+
+// Uncomment and configure CORS if needed
 // router.use(
-//     cors({
-//         credentials: true,
-//         origin: 'http://localhost:5173'
-//     })
-// )
+//   cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+//   })
+// );
 
-router.get('/', test)
-router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.get('/profile', getProfile)
+// Auth Routes
+router.get('/', test);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', getProfile);
 router.post('/logout', logoutUser);
 
-// User Profile section
-// router.post('/', userController.createUser);
-// router.get('/', userController.getAllUsers);
+// User CRUD Routes
+router.get('/get_users', getAllUsers);
+router.get('/user_profile/:id', getUserById);
+router.post('/edit_profile/:id', updateUser);
+router.post('/delete_profile/:id', deleteUser);
 
+// Service CRUD Routes
+router.post('/create_service', serviceController.createService);
+router.get('/get_all_services', serviceController.getAllServices);
+router.get('/get_service/:userid', serviceController.getServiceByUserId);
+router.post('/update_service/:id', serviceController.updateService);
+router.post('/delete_service/:id', serviceController.deleteService);
 
-// Create a new user
-router.post('/create_user', userController.createUser);
+// work CRUD Routes
+router.post('/create_work', workController.createWork);
+router.get('/get_all_works', workController.getAllWorks);
+router.get('/get_work/:userid', workController.getWorkByUserId);
+router.post('/update_work/:id', workController.updateWork);
+router.post('/delete_work/:id', workController.deleteWork);
 
-// Get all users
-router.get('/get_users', userController.getAllUsers);
-// Get a specific user profile
-router.get('/user_profile/:id', userController.getUserById);
+// review CRUD Routes
+router.post('/create_review', reviewController.createReview);
+router.get('/get_all_reviews', reviewController.getAllReviews);
+router.get('/get_review/:userid', reviewController.getReviewsByUserId);
+router.post('/update_review/:id', reviewController.updateReview);
+router.post('/delete_review/:id', reviewController.deleteReview);
 
-// Update a user profile
-router.post('/edit_profile/:id', userController.updateUser);
-
-// Delete a user profile
-router.post('/delete_profile/:id', userController.deleteUser);
-
-module.exports = router
+module.exports = router;
